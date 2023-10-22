fbdd553028f1b4c12f7e955a12d5643c3b6f457f
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-10-22 11:11:02 +0500

    React Form-successfully designed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
+  let handleSubmit = (e) => {
+
+    e.preventDefault();
+    alert(`The name you entered was:  ${name}`)
+
+  };
   return (
     <div className="App">
       <h1>This is React App</h1>
+      <div>
+        <h2 style={{ textAlign: "left" }}>Form:</h2>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Enter your name:
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+            <input type="submit" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
